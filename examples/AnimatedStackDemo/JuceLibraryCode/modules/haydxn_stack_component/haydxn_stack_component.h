f6275174679538b9f40fa5c5442c1018db08e997
Adam Wilson <adam@codegarden.co.uk>
2015-05-19 23:00:19 +0100

    AnimatedListBox (to be renamed) with itemClicked callback

---
+++ examples/AnimatedStackDemo/JuceLibraryCode/modules/haydxn_stack_component/haydxn_stack_component.h
@@ -0,0 +1,5 @@
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
+
+#include "../../../../../modules/haydxn_stack_component/haydxn_stack_component.h"
+
---
+++ examples/AnimatedStackDemo/Source/MainComponent.cpp
@@ -0,0 +1,66 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+
+
+//==============================================================================
+MainContentComponent::MainContentComponent()
+{
+    setSize (600, 400);
+    ValueTree data (AnimatedListBoxIds::data);  
+    
+    ValueTree node1 (AnimatedListBoxIds::node);
+    node1.setProperty (AnimatedListBoxIds::title, "Slide", nullptr);
+    node1.setProperty (AnimatedListBoxIds::description, "Sliding Animator", nullptr);
+    data.addChild (node1, -1, nullptr);
+
+    ValueTree node2 (AnimatedListBoxIds::node);
+    node2.setProperty (AnimatedListBoxIds::title, "Shutter", nullptr);
+    node2.setProperty (AnimatedListBoxIds::description, "Shutter Animator", nullptr);
+    data.addChild (node2, -1, nullptr);
+
+    ValueTree node3 (AnimatedListBoxIds::node);
+    node3.setProperty (AnimatedListBoxIds::title, "Fade", nullptr);
+    node3.setProperty (AnimatedListBoxIds::description, "Fade Animator", nullptr);
+    data.addChild (node3, -1, nullptr);
+
+    addAndMakeVisible (listBox = new AnimatedListBox (data,
+        // itemClicked callback function
+        [this] (int row, ListBox* source, ValueTree node, const MouseEvent &e)
+        {
+            Rectangle<int> rowPosition = source->getRowPosition(row, false);
+            DBG (rowPosition.toString());
+            DBG (node.toXmlString());
+            DBG ("row number clicked: " << row);
+        }
+    ));
+    DBG (data.toXmlString());
+    listBox->setBounds (getBounds());
+}
+
+MainContentComponent::~MainContentComponent()
+{
+}
+
+void MainContentComponent::paint (Graphics& g)
+{
+    g.fillAll (Colour (0xff001F36));
+
+    g.setFont (Font (16.0f));
+    g.setColour (Colours::white);
+    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+}
+
+void MainContentComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
