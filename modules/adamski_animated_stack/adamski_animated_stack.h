49b13cb1a7d89e167dcd09be3ec1602c0e8b94b9
Cyril Stoller <cyril.stoller@gmail.com>
2016-09-10 02:50:52 +0200

    updated module h-files with necessary JUCE_MODULE_DECLARATION block
    Also deleted old juce_module_info file

--- modules/adamski_animated_stack/adamski_animated_stack.h
+++ modules/adamski_animated_stack/adamski_animated_stack.h
@@ -1,13 +1,36 @@
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               adamski_animated_stack
+  vendor:           adamski
+  version:          0.1.0
+  name:             Animated Stack Component
+  description:      Extension of Stack Component allowing different animations to be used per component
+  website:          https:/github.com/adamski/AnimatedStack
+  license:          The MIT License (MIT)
+
+  dependencies:     juce_core juce_gui_basics juce_graphics juce_events haydxn_stack_component
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
 
 #ifndef ADAMSKI_ANIMATED_STACK_H_INCLUDED
 #define ADAMSKI_ANIMATED_STACK_H_INCLUDED
 
 //=============================================================================
-#include <modules/juce_core/juce_core.h>
-#include <modules/juce_events/juce_events.h>
-#include <modules/juce_graphics/juce_graphics.h>
-#include <modules/juce_gui_basics/juce_gui_basics.h>
-#include <modules/haydxn_stack_component/haydxn_stack_component.h>
+#include <juce_core/juce_core.h>
+#include <juce_events/juce_events.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <haydxn_stack_component/haydxn_stack_component.h>
 
 class AnimatedStackComponent;
 class StackAnimator;
